52128607ef9d5cbe6d5a5b24208150b170f418d3
Imnotcoderdude <topsolo7@naver.com>
2025-04-30 01:07:15 +0900

    init commit

---
+++ src/main/java/com/sparta/limited/user_coupon_service/UserCouponServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.limited.user_coupon_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCouponServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCouponServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/limited/user_coupon_service/UserCouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.limited.user_coupon_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCouponServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7400dd97d18af7d143a6fd1722f9e28e4cae8802
Imnotcoderdude <topsolo7@naver.com>
2025-04-30 22:39:59 +0900

    ♻️ refactor [#1] : 서비스 로직 리팩토링 - 서비스에서 조절하는 것이 아닌 lua 스크립트로 한번에 처리하기 떄문에 불필요한 흐름제거

--- src/main/java/com/sparta/limited/user_coupon_service/application/service/UserCouponService.java
+++ src/main/java/com/sparta/limited/user_coupon_service/application/service/UserCouponService.java
@@ -3,7 +3,6 @@ package com.sparta.limited.user_coupon_service.application.service;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,16 +10,10 @@ public class UserCouponService {
 
     private final RedisCouponIssueService redisCouponIssueService;
 
-    @Transactional
     public void creatUserCoupon(
         UUID couponId,
         Long userId
     ) {
-        redisCouponIssueService.quantityAndDuplicate(couponId, userId);
-
-        redisCouponIssueService.userCouponCreateEvent(couponId, userId);
-
-        redisCouponIssueService.decreaseCouponQuantityEvent(couponId);
-
+        redisCouponIssueService.createIssueCoupon(couponId, userId);
     }
 }

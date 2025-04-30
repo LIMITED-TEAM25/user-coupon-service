a08a7eabe7d73a38b6c1543de9a1b6627139f830
Imnotcoderdude <topsolo7@naver.com>
2025-04-30 22:34:42 +0900

    ♻️ refactor [#1] : lua 스크립트 스텝 추가 - 재고감소, 사용자 쿠폰 발급 큐에 데이터 삽입 추가

---
+++ src/main/resources/lua/createIssueCoupon.lua
@@ -0,0 +1,22 @@
+-- KEYS[1]=coupon:{id}:quantity
+-- KEYS[2]=coupon:{id}:users
+-- KEYS[3]=user-coupon:queue
+-- KEYS[4]=coupon-quantity-decrease:queue
+-- ARGV[1]=userId
+-- ARGV[2]=couponId|userId
+-- ARGV[3]=couponId
+
+local remain = redis.call("DECR", KEYS[1])
+if not remain or remain < 0 then
+  redis.call("INCR", KEYS[1])
+  return "OUT_OF_STOCK"
+end
+
+if redis.call("SADD", KEYS[2], ARGV[1]) == 0 then
+  redis.call("INCR", KEYS[1])
+  return "DUPLICATED"
+end
+
+redis.call("RPUSH", KEYS[3], ARGV[2])
+redis.call("RPUSH", KEYS[4], ARGV[3])
+return {ok=tostring(remain)}
